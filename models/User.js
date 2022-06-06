1f5bf31cd6d2bf7f9661e141d7082b75a75f26d9
Youssef04544 <youssefbenyedder04@gmail.com>
2022-06-06 19:37:53 +0100

    changed the hashing and put it in the model using mongoose middleware UserSchema.pre

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { string } = require("joi");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = mongoose.Schema({
   name: {
@@ -23,6 +24,11 @@ const UserSchema = mongoose.Schema({
     required: [true, "Please provide name"],
     minLength: 6,
   },
+});
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model("User", UserSchema);
