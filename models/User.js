3396a72e52333a985c05dd0ecc2672c0ef75ba6a
Youssef04544 <youssefbenyedder04@gmail.com>
2022-06-07 21:56:19 +0100

    Created job model

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ const { string } = require("joi");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-require("dotenv");
 
 const UserSchema = mongoose.Schema({
   name: {
